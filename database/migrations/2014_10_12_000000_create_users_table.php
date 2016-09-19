391599665ea2c53de3a26d96eec972ceaf016bdd
fectda <fectda@gmail.com>
2016-09-18 21:21:03 -0500

    migraciones de roles y permissions

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,11 +15,20 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
+            $table->string('name')->unique();
+
+            $table->string('first_name');
+            $table->string('last_name');
+
             $table->string('email')->unique();
-            $table->string('password');
+            $table->string('password', 60);
             $table->rememberToken();
+
+            $table->boolean('active')->default(false);
+            // $table->dateTime('last_login')->nullable();
+
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
